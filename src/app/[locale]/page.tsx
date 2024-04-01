5b3b300b2b6991c9c5888556bba22c486ff70452
aminahbl <aminahbl.github@37e.uk>
2024-04-01 18:56:28 +0200

    refactor: strip out function tabs from homepage and add translation feature only

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,39 +1,21 @@
 import { useTranslations } from "next-intl"
 
 import { PageShell } from "@/components/layout"
-import FeatureSelectorTabs from "@/features/FeatureSelectorTabs"
+import TranslationInput from "@/features/translation/TranslationInput"
+import TranslationResults from "@/features/translation/TranslationResults"
 
 // As we're using query parameters that are only known
 // at request time, we need to make sure we're using
 // dynamic rendering (i.e. no SSG).
 export const dynamic = "force-dynamic"
 
-type Props = {
-  searchParams?: {
-    view?: "search" | "translate"
-    page?: string
-  }
-}
-
-export default function Home({ searchParams }: Props) {
+export default function Home() {
   const t = useTranslations("Home")
 
-  const tabIndex = searchParams?.view === "translate" ? 1 : 0
-
   return (
-    <PageShell title={t("title")} visuallyHiddenTitle>
-      <FeatureSelectorTabs
-        tabIndex={tabIndex}
-        tabLabels={{ 0: t("search.tabLabel"), 1: t("translation.tabLabel") }}
-        headings={{
-          search: t("search.heading"),
-          translation: t("translation.heading"),
-        }}
-        placeholders={{
-          search: t("search.placeholder"),
-          translation: t("translation.placeholder"),
-        }}
-      />
+    <PageShell h1={t("translation.heading")} visuallyHiddenH1>
+      <TranslationInput placeholder={t("translation.placeholder")} />
+      <TranslationResults />
     </PageShell>
   )
 }
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -12,48 +12,52 @@ export default function Navigation() {
   const navItems = useNavItems()
   const t = useTranslations("navigation")
   return (
-    <AppBar
-      component="nav"
-      elevation={1}
-      sx={{ py: 1, backgroundColor: "white" }}
-    >
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <LocalLink href="/" sx={{ textDecoration: "none" }}>
-            DHARMAMITRA
-          </LocalLink>
-        </Typography>
+    <>
+      <AppBar
+        component="nav"
+        elevation={1}
+        sx={{ py: 1, backgroundColor: "white" }}
+      >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <LocalLink href="/" sx={{ textDecoration: "none" }}>
+              DHARMAMITRA
+            </LocalLink>
+          </Typography>
 
-        <NavMobileMenu
-          navItems={navItems}
-          messages={{
-            ariaButton: t("aria-mobile-button"),
-            ariaMenu: t("aria-mobile-menu"),
-          }}
-        >
-          <LocaleSelector />
-        </NavMobileMenu>
+          <NavMobileMenu
+            navItems={navItems}
+            messages={{
+              ariaButton: t("aria-mobile-button"),
+              ariaMenu: t("aria-mobile-menu"),
+            }}
+          >
+            <LocaleSelector />
+          </NavMobileMenu>
 
-        <Box
-          sx={{
-            display: { xs: "none", sm: "flex" },
-            alignItems: "center",
-            gap: 2,
-          }}
-        >
-          <Box sx={{ display: "flex", gap: 1 }}>
-            {navItems.map((item) => {
-              const { id, label, href } = item
-              return (
-                <LocalLink key={id} href={href} variant="text-button">
-                  {label}
-                </LocalLink>
-              )
-            })}
+          <Box
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Box sx={{ display: "flex", gap: 1 }}>
+              {navItems.map((item) => {
+                const { id, label, href } = item
+                return (
+                  <LocalLink key={id} href={href} variant="text-button">
+                    {label}
+                  </LocalLink>
+                )
+              })}
+            </Box>
+            <LocaleSelector />
           </Box>
-          <LocaleSelector />
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      {/* navbar offset */}
+      <div style={{ width: "100%", height: 80 }} />
+    </>
   )
 }
