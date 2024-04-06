cb4632a4ae12df64fd52d129263fbca8452d580b
aminahbl <aminahbl.github@37e.uk>
2024-04-06 14:11:59 +0200

    refactor: remove unneeded props from tab component

--- src/components/features/FeatureSelectorTabs.tsx
+++ src/components/features/FeatureSelectorTabs.tsx
@@ -132,7 +132,7 @@ export default function FeatureSelectorTabs({
             {headings.translation}
           </Typography>
 
-          <TranslationInput placeholder={placeholders.translation!} />
+          <TranslationInput />
 
           <TranslationResults />
         </FeatureTabPanel>
