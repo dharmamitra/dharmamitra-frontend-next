192c32243f2ded106a27548d75514b631d5333bb
aminahbl <aminahbl.github@37e.uk>
2024-04-07 15:11:19 +0200

    feat: port and develop team page

--- src/app/[locale]/team/page.tsx
+++ src/app/[locale]/team/page.tsx
@@ -1,25 +1,109 @@
+import Image from "next/image"
 import { useTranslations } from "next-intl"
 import { Typography } from "@mui/material"
-import Avatar from "@mui/material/Avatar"
-import { deepOrange, deepPurple } from "@mui/material/colors"
+import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 
 import { PageShell } from "@/components/layout"
+
+import members from "./data"
 
 export default function Home() {
   const t = useTranslations("Team")
 
   return (
-    <PageShell>
+    <PageShell sx={{ mb: 12 }}>
       <Typography variant="h1" component="h1">
         {t("h1")}
       </Typography>
 
-      <Stack direction="row" spacing={2}>
-        <Avatar>H</Avatar>
-        <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
-        <Avatar sx={{ bgcolor: deepPurple[500] }}>P</Avatar>
-      </Stack>
+      <section style={{ marginBlockEnd: "4rem" }}>
+        <Typography variant="h2" mb={4}>
+          {t("present.h2")}
+        </Typography>
+
+        <Stack
+          direction="row"
+          spacing={4}
+          rowGap={8}
+          useFlexGap
+          flexWrap="wrap"
+        >
+          {members.current.map((member) => {
+            const { id, name, roles, image } = member
+            return (
+              <Box key={id}>
+                <Image
+                  src={image}
+                  alt={name}
+                  width={200}
+                  height={200}
+                  style={{ borderRadius: "50%" }}
+                />
+
+                <Typography align="center" variant="h6" component="p">
+                  {name}
+                </Typography>
+                {roles &&
+                  roles.map(({ i18nRoleKey }, index) => (
+                    <Typography
+                      align="center"
+                      key={id + "-" + i18nRoleKey}
+                      color="grey"
+                    >
+                      {t(`roles.${i18nRoleKey}`)}
+                      {index < roles.length - 1 && ","}
+                    </Typography>
+                  ))}
+              </Box>
+            )
+          })}
+        </Stack>
+      </section>
+
+      <section style={{ marginBlockEnd: "4rem" }}>
+        <Typography variant="h2" mb={4}>
+          {t("past.h2")}
+        </Typography>
+
+        <Stack
+          direction="row"
+          spacing={4}
+          rowGap={8}
+          useFlexGap
+          flexWrap="wrap"
+        >
+          {members.past.map((member) => {
+            const { id, name, roles, image } = member
+            return (
+              <Box key={id}>
+                <Image
+                  src={image}
+                  alt={name}
+                  width={200}
+                  height={200}
+                  style={{ borderRadius: "50%" }}
+                />
+
+                <Typography align="center" variant="h6" component="p">
+                  {name}
+                </Typography>
+                {roles &&
+                  roles.map(({ i18nRoleKey }, index) => (
+                    <Typography
+                      align="center"
+                      key={id + "-" + i18nRoleKey}
+                      color="grey"
+                    >
+                      {t(`roles.${i18nRoleKey}`)}
+                      {index < roles.length - 1 && ","}
+                    </Typography>
+                  ))}
+              </Box>
+            )
+          })}
+        </Stack>
+      </section>
     </PageShell>
   )
 }
