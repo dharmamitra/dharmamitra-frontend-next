f08d6d54c963043b36a53783edfd87bd68edb1ff
aminahbl <aminahbl.github@37e.uk>
2024-04-11 21:26:35 +0200

    feat: add internationalized 404 page

--- src/components/LocalLink.tsx
+++ src/components/LocalLink.tsx
@@ -3,31 +3,54 @@
 import React, { ComponentProps } from "react"
 import { useSelectedLayoutSegment } from "next/navigation"
 import { Link as MuiLink, SxProps, Theme } from "@mui/material"
-import { alpha, styled } from "@mui/material/styles"
+import { alpha, darken, styled } from "@mui/material/styles"
 
 import { Link as NavigationLink } from "@/navigation"
 
-const LocalLinkButton = styled(NavigationLink)(({ theme }) => ({
+const LocalLinkButton = styled(NavigationLink, {
+  shouldForwardProp: (prop) => prop !== "variant" && prop !== "sx",
+})<{
+  variant?: "button" | "outlined-button" | "text-button"
+}>(({ theme, variant }) => ({
   display: "inline-flex",
   alignItems: "center",
   justifyContent: "center",
   gap: theme.spacing(1),
   paddingBlock: theme.spacing(1),
   paddingInline: theme.spacing(1.5),
-  color: theme.palette.primary.main,
   fontSize: theme.typography.body2.fontSize,
   fontWeight: 500,
   lineHeight: 1.75,
   textTransform: "uppercase",
   textDecoration: "none",
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: "transparent",
   transition: theme.transitions.create(["background-color"], {
     duration: theme.transitions.duration.short,
   }),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.primary.main, 0.04),
-  },
+  ...(variant === "button" && {
+    color: theme.palette.common.white,
+    boxShadow:
+      "0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)",
+    backgroundColor: theme.palette.primary.main,
+    "&:hover": {
+      backgroundColor: darken(theme.palette.primary.main, 0.15),
+    },
+  }),
+  ...(variant === "text-button" && {
+    color: theme.palette.primary.main,
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.primary.main, 0.04),
+    },
+  }),
+  ...(variant === "outlined-button" && {
+    color: theme.palette.primary.main,
+    backgroundColor: "transparent",
+    border: `1px solid ${theme.palette.primary.main}`,
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.primary.main, 0.04),
+    },
+  }),
 }))
 
 const NavigationLinkForwardRef = React.forwardRef<
@@ -46,8 +69,8 @@ export default function LocalLink({
   ...rest
 }: ComponentProps<typeof NavigationLink> & {
   sx?: SxProps<Theme>
-  // TODO: Add support for "button" and "outline-button"
-  variant?: "link" | "button" | "outline-button" | "text-button"
+  // TODO: Add support for "button" and "outlined-button"
+  variant?: "link" | "button" | "outlined-button" | "text-button"
 }) {
   const selectedLayoutSegment = useSelectedLayoutSegment()
   const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : "/"
@@ -71,7 +94,9 @@ export default function LocalLink({
         </MuiLink>
       ) : (
         <LocalLinkButton
+          sx={sx}
           href={href}
+          variant={variant}
           aria-current={isActive ? "page" : undefined}
           {...rest}
         >
