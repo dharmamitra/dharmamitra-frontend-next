5c9c4b39343408d75acee7f6f65de548ea32b0df
aminahbl <aminahbl.github@37e.uk>
2024-07-18 21:39:25 +0100

    build test

--- src/components/features/search/SearchResults/SearchResults.tsx
+++ src/components/features/search/SearchResults/SearchResults.tsx
@@ -1,27 +1,28 @@
 "use client"
 
-import React from "react"
-import Box from "@mui/material/Box"
+// import React from "react"
+// import Box from "@mui/material/Box"
 
-import useSearchCommonParams from "@/hooks/search/useSearchCommonParams"
-import { defaultSearchTarget, SearchTarget } from "@/utils/api/search/local"
+// import useSearchCommonParams from "@/hooks/search/useSearchCommonParams"
+// import { defaultSearchTarget, SearchTarget } from "@/utils/api/search/local"
 
-import ParallelQueryResults from "./ParallelQueryResults"
-import PrimaryQueryResults from "./PrimaryQueryResults"
+// import ParallelQueryResults from "./ParallelQueryResults"
+// import PrimaryQueryResults from "./PrimaryQueryResults"
 
-const results: Record<SearchTarget, JSX.Element> = {
-  parallel: <ParallelQueryResults />,
-  primary: <PrimaryQueryResults />,
-  secondary: <div />, // TODO
-}
+// const results: Record<SearchTarget, JSX.Element> = {
+//   parallel: <ParallelQueryResults />,
+//   primary: <PrimaryQueryResults />,
+//   secondary: <div />, // TODO
+// }
 
 export default function SearchResults() {
-  const { searchTarget, setSearchInput } = useSearchCommonParams()
+  // const { searchTarget, setSearchInput } = useSearchCommonParams()
 
-  if (!searchTarget || (searchTarget && !results[searchTarget])) {
-    setSearchInput(defaultSearchTarget)
-    return <Box sx={{ pt: 4 }}>{results[defaultSearchTarget]}</Box>
-  }
+  // if (!searchTarget || (searchTarget && !results[searchTarget])) {
+  //   setSearchInput(defaultSearchTarget)
+  //   return <Box sx={{ pt: 4 }}>{results[defaultSearchTarget]}</Box>
+  // }
 
-  return <Box sx={{ pt: 4 }}>{results[searchTarget]}</Box>
+  // return <Box sx={{ pt: 4 }}>{results[searchTarget]}</Box>
+  return null
 }
