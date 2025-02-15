a55adbcce1ab25196640bc4346d5da78a7f45dce
aminahbl <aminahbl.github@37e.uk>
2025-02-15 16:37:26 +0000

    wip: restore sentry tunnel route

--- next.config.mjs
+++ next.config.mjs
@@ -45,7 +45,7 @@ const sentryConfig = {
   reactComponentAnnotation: {
     enabled: true,
   },
-  tunnelRoute: getBasePath() + "/monitoring-tunnel",
+  tunnelRoute: "/monitoring-tunnel",
   hideSourceMaps: true,
   disableLogger: false,
   sourcemaps: {
