567bad1476e9158480994273a6eb6e506499177b
sebastian-nehrdich <nehrdbsd@gmail.com>
2025-07-10 11:18:02 -0700

    feat(i18n): add Tibetan localization

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,11 @@
-export const SUPPORTED_LOCALES = ["en", "zh-Hans", "zh-Hant"] as const
+export const SUPPORTED_LOCALES = ["en", "ko", "ja", "bo", "zh-Hans", "zh-Hant"] as const
 export const defaultLocale = "en" as const
 
 export const localePrefixs: Record<(typeof SUPPORTED_LOCALES)[number], string> = {
   en: "/en",
+  ko: "/ko",
+  ja: "/ja",
+  bo: "/bo",
   "zh-Hans": "/zh",
   "zh-Hant": "/zh-hant",
 }
--- src/utils/searchExamples.ts
+++ src/utils/searchExamples.ts
@@ -31,6 +31,24 @@ const en = [
   "Looking with kindly eyes",
   "Subjects for frequent recollection",
 ]
+const ko = [
+  // TODO
+  ...en,
+]
+const ja = [
+  // TODO
+  ...en,
+]
+const bo = [
+  "སངས་རྒྱས་ལ་དགའ་བ།",
+  "སེམས་ཅན་ཐམས་ཅད་ལ་བྱམས་པ།",
+  "བཟོད་པའི་ཡོན་ཏན།",
+  "སྟོང་ཉིད་ཀྱི་དོན།",
+  "བྱང་ཆུབ་ཀྱི་སེམས།",
+  "The Buddha’s compassion",
+  "Definition of peace",
+  "The benefits of giving",
+]
 const zhHans = [
   // TODO
   ...en,
@@ -42,6 +60,9 @@ const zhHant = [
 
 export const exampleSearchStrings: Record<SupportedLocale, string[]> = {
   en,
+  ko,
+  ja,
+  bo,
   "zh-Hans": zhHans,
   "zh-Hant": zhHant,
 }
