3b23742e888e8a8998a1291da4110ad8b9bf8ced
aminahbl <aminahbl.github@37e.uk>
2025-07-12 22:19:48 +0200

    feat: #175 max results warning & info

--- src/utils/searchExamples.ts
+++ src/utils/searchExamples.ts
@@ -1,19 +1,20 @@
 const en = [
   "The definition of cetanā",
-  "A verse that compares the suffering of ordinary people with a hair on the palm of the hand",
+  "Texts that compare the suffering of ordinary people",
   "Can an arahat feel doubts?",
   "Verses similar to “yena yena vikalpena yadyad vastu vikalpyate”",
-  "A passage describing the Buddha being asked to remain in the world for a kalpa or longer",
+  "A passage describing the Buddha being asked to remain in the world",
+  "The four noble truths",
   "What is the difference between paññā and viññāṇa?",
-  "what is the Tathāgatagarbha / 如來藏?",
-  "How does ignorance (avijjā) function in the chain of Dependent Origination (paṭiccasamuppāda)?",
+  "What is the Tathāgatagarbha / 如來藏?",
+  "How does avijjā function in the chain of Dependent Origination?",
   "Who was Angulimala?",
   "A verse comparing consciousness to a flowing river",
   "An explanation of the five strengths",
   "What are the 37 bodhipakṣa-dharmas?",
   "云何三十七菩提分法？",
   "a definition of the term “change”",
-  "What is the 'threefold truth' (一心三觀)",
+  "What is the ‘threefold truth’ (一心三觀)?",
 ]
 
 const zhHans = [
