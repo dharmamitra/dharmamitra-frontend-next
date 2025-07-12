101b01005b7f66685dda82709af923532adf747b
aminahbl <aminahbl.github@37e.uk>
2025-07-12 15:36:42 +0200

    refactor: #174 english resaerch styling

--- src/components/memoized-markdown.tsx
+++ src/components/memoized-markdown.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useMemo } from "react"
 import ReactMarkdown from "react-markdown"
+import { Link, Typography } from "@mui/material"
 import { marked } from "marked"
 
 /**
@@ -14,7 +15,39 @@ function parseMarkdownIntoBlocks(markdown: string): string[] {
 
 const MemoizedMarkdownBlock = memo(
   ({ content }: { content: string }) => {
-    return <ReactMarkdown>{content}</ReactMarkdown>
+    return (
+      <ReactMarkdown
+        components={{
+          h2: ({ node, ...props }) => (
+            <Typography
+              component="h2"
+              fontWeight={500}
+              sx={{ mt: 2, mb: 0.5, fontSize: "1.5rem !important" }}
+              {...props}
+            />
+          ),
+          h3: ({ node, ...props }) => (
+            <Typography variant="h3" fontWeight={500} my={2} {...props} />
+          ),
+          h4: ({ node, ...props }) => (
+            <Typography variant="h4" fontWeight={500} my={2} {...props} />
+          ),
+          h5: ({ node, ...props }) => (
+            <Typography variant="h5" fontWeight={500} my={2} {...props} />
+          ),
+          // TODO: Hardcoded color value need to be extracted from theme
+          p: ({ node, ...props }) => <Typography sx={{ color: "#222", mb: 2 }} {...props} />,
+          strong: ({ node, ...props }) => (
+            <Typography component="strong" fontWeight={500} color="primary" {...props} />
+          ),
+          a: ({ node, href, ...props }) => (
+            <Link href={href} target="_blank" rel="noopener noreferrer" {...props} />
+          ),
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    )
   },
   (prevProps, nextProps) => {
     if (prevProps.content !== nextProps.content) return false
