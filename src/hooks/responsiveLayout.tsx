893895d48c7d1b3b66aba9fa0c1233baf7e570e6
aminahbl <aminahbl.github@37e.uk>
2025-07-28 20:09:13 +0100

    fix: translation option layout

--- src/hooks/responsiveLayout.tsx
+++ src/hooks/responsiveLayout.tsx
@@ -4,34 +4,24 @@ import useMediaQuery from "@mui/material/useMediaQuery"
 import { getSettingPriotiryGroups } from "@/utils"
 
 export const useResponsiveSizes = () => {
-  const isSmTablet = useMediaQuery("(min-width: 510px)")
-  const isLgTablet = useMediaQuery("(min-width: 760px)")
-  const isSmDesktop = useMediaQuery("(min-width: 975px)")
-  const isLgDesktop = useMediaQuery("(min-width: 1260px)")
+  const isLgDesktop = useMediaQuery("(min-width: 1395px)")
 
   return {
-    isSmTablet,
-    isLgTablet,
-    isSmDesktop,
     isLgDesktop,
-    isSingleColLayout: !isLgTablet,
+    isSingleColLayout: !isLgDesktop,
   }
 }
 
 export const useResponsiveOptions = <T extends string>(options: T[]) => {
-  const { isSmTablet, isLgTablet, isSmDesktop, isLgDesktop, isSingleColLayout } =
-    useResponsiveSizes()
+  const { isLgDesktop, isSingleColLayout } = useResponsiveSizes()
 
   return React.useMemo<[T[], T[]]>(() => {
     let noOfPrimaryItems = 1
-    if (isSingleColLayout && isSmTablet) noOfPrimaryItems = 2
-    if (!isSingleColLayout && isLgTablet) noOfPrimaryItems = 1
-    if (!isSingleColLayout && isSmDesktop) noOfPrimaryItems = 2
     if (isLgDesktop) noOfPrimaryItems = 3
 
     return getSettingPriotiryGroups<T>({
       setting: options,
       noOfPrimaryItems,
     })
-  }, [options, isSmTablet, isLgTablet, isSmDesktop, isLgDesktop, isSingleColLayout])
+  }, [options, isLgDesktop, isSingleColLayout])
 }
